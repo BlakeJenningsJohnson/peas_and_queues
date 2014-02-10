87ca89385b896eb592fb5ae5cbbf337f749b0936
Your Name <blake.jennings@gmail.com>
2014-02-10 10:52:14 -0800

    second commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PeasAndQueues::Application.config.secret_key_base = '432ad822375ff68522f35924783e90b79d345dc41ec7b4f2e3e02929fa34a8f3d1cf10141c8f978ee5a118a3ce4f67fbef892054c0a484e1c997b3d654c96e2d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PeasAndQueues::Application.config.session_store :cookie_store, key: '_peas_and_queues_session'
