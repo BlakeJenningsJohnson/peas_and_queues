dc1abddc5e81d4f8ea85b976cab6f03cdae27666
Davida Marion <davidamarion@gmail.com>
2014-02-11 21:42:22 -0800

    cleans up some unnecessary routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ PeasAndQueues::Application.routes.draw do
   root  'welcome#index'
   match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
 
-  get '/users', to: 'users#show'
-  resources :events
+  get '/users',                     to: 'users#show'
+  get '/events',                    to: 'events#index'
+  get '/events/new',                to: 'events#new', as: :new_event
+  post '/events',               to: 'events#create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
