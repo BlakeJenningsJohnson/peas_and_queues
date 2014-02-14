24818e8b000a9c444a3a3235dc9f7f9e8a2cdfcc
Your Name <blake.jennings@gmail.com>
2014-02-14 09:12:29 -0800

    weather change

--- app/models/weather_report.rb
+++ app/models/weather_report.rb
@@ -1,9 +1,7 @@
 class WeatherReport
 
   def self.get_weather
-    # cache('weather_report', expires_in: 1.hour) do
-      ForecastIO.forecast(47.6097, -122.3331)
-    # end
+    ForecastIO.forecast(47.6097, -122.3331)
   end
 
   def self.current_weather
