10fcbb7be8450b76596fda677a310c2313d82ecc
Davida Marion <davidamarion@gmail.com>
2014-02-14 15:38:23 -0800

    fixes bug that did not exist. yay!

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,6 @@
 class EventsController < ApplicationController
   before_action :find_event, only: [:rsvp, :edit, :update]
 
-
   def index
     @events = Event.all
     @events_by_date = @events.group_by(&:date)
@@ -15,12 +14,26 @@ class EventsController < ApplicationController
   def create
     @event = Event.new(event_params)
     @event.host_id = current_user.id
-    if @event.save
-      weather = WeatherReport.get_forecast('Time.new(@event.date.strftime("%Y, %m, %d")')
-      @event.update(temperature: weather.temperature.round, conditions: weather.icon, time: @event.time.strftime("%I:%M:%S %p"))
+    if @event.save && (@event.date - Date.today <= 7)
+      weather = WeatherReport.get_forecast('Time.new(@event.date.strftime("%Y, %m, %d")')[@event.time.strftime('%H').to_i]
+      @event.update(temperature: weather.apparentTemperature.round, conditions: weather.icon)
+      if @event.recurring?
+        create_recurring
+      end
       redirect_to events_path
     else
       render :new
+    end
+  end
+
+  def create_recurring
+    n = 1
+    52.times do
+      @event = Event.new(event_params)
+      @event.host_id = current_user.id
+      @event.update(date: (@event.date + (7*n)))
+      @event.save
+      n = n+1
     end
   end
 
@@ -46,6 +59,13 @@ class EventsController < ApplicationController
   end
 
   def event_params
-    params.require(:event).permit(:name, :description, :date, :time, :conditions, :temperature, :host_id)
+    params.require(:event).permit(:name,
+                                  :description,
+                                  :date,
+                                  :time,
+                                  :conditions,
+                                  :temperature,
+                                  :host_id,
+                                  :recurring)
   end
 end
