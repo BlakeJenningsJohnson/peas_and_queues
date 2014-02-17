69b1266eb9cf47e94ab5975a00b4880344dc08ab
Davida Marion <davidamarion@gmail.com>
2014-02-16 20:27:44 -0800

    events add is now a popup

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,6 +5,7 @@ class EventsController < ApplicationController
     @events = Event.all
     @events_by_date = @events.group_by(&:date)
     @date = params[:date] ? Date.parse(params[:date]) : Date.today
+    @event = Event.new
   end
 
   def new
