eff4efb64afdd00a6033824be9efa6240aa8bbf8
Davida Marion <davidamarion@gmail.com>
2014-02-16 18:38:04 -0800

    extracts out actual creation of events to DRY

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -12,11 +12,11 @@ class EventsController < ApplicationController
   end
 
   def create
-    @event = Event.new(event_params)
-    @event.host_id = current_user.id
+    make_event
     if @event.save && (@event.date - Date.today <= 7)
       weather = WeatherReport.get_forecast('Time.new(@event.date.strftime("%Y, %m, %d")')[@event.time.strftime('%H').to_i]
       @event.update(temperature: weather.apparentTemperature.round, conditions: weather.icon)
+      current_user.events << @event
       if @event.recurring?
         create_recurring
       end
@@ -26,13 +26,18 @@ class EventsController < ApplicationController
     end
   end
 
+  def make_event
+    @event = Event.new(event_params)
+    @event.host_id = current_user.id
+  end
+
   def create_recurring
     n = 1
     52.times do
-      @event = Event.new(event_params)
-      @event.host_id = current_user.id
+      make_event
       @event.update(date: (@event.date + (7*n)))
       @event.save
+      current_user.events << @event
       n = n+1
     end
   end
