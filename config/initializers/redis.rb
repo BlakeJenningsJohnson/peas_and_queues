a726ebf26a0bef85371472e337a91c24401e1f06
Davida Marion <davidamarion@gmail.com>
2014-02-18 13:56:00 -0800

    trying to fix redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,5 @@
+ENV["REDISTOGO_URL"] ||= "redis://username:password@host:6379/"
+
 if Rails.env.production? 
   uri = URI.parse(ENV["REDISTOGO_URL"])
   Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
