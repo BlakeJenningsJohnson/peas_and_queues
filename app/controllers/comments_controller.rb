597b676bba6519a62b94db25a3a367ff7cfaf5f1
Bookis Smuin <bookis.smuin@gmail.com>
2014-02-21 14:28:40 -0800

    adds ajax to comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,11 @@ class CommentsController < ApplicationController
     @comment = @post.comments.create(comment_params)
     if @comment.save
       @comment.update(user_id: current_user.id)
-      redirect_to :back
+      respond_to do |format|
+        format.html { redirect_to :back }
+        format.json { render json: @comment.as_json(only: :comment, include: :user) }
+      end
+      
     end
   end
 
