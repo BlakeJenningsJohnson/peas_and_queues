5d7cc0588b0bfc6d30f38a1265e9df15d6000bc1
Davida Marion <davidamarion@gmail.com>
2014-02-20 22:06:39 -0800

    readded in hook

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,11 @@
-# ENV['REDISTOGO_URL'] ||= "redis://username:password@host:6379/"
+ENV['REDISTOGO_URL'] ||= "redis://username:password@host:6379/"
+
+# if Rails.env.production? 
+#   uri = URI.parse(ENV['REDISTOGO_URL'])
+#   Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+# elsif Rails.env.development?
+#   Resque.redis = Redis.new(:host => 'localhost', :port => '6379')
+# end
 
 if Rails.env.development?
   Redis.current = Redis.new
