6168d6e58ec2699f6016a6a1412801a040efe850
Davida Marion <davidamarion@gmail.com>
2014-02-21 22:29:39 -0800

    testing tool helper method;

--- app/helpers/tools_helper.rb
+++ app/helpers/tools_helper.rb
@@ -1,3 +1,15 @@
 module ToolsHelper
+  def availability(tool_id, user_id=nil)
+    tool = Tool.find(tool_id)
+    if tool.status == 'available'
+      @response = 'Available!'
+    elsif tool.status == 'on hold' && user_id == tool.user_id
+      @reponse = 'That tool is here for you!'
+    elsif user_id == tool.user_id
+      @response  = "You've GOT this tool!"
+    else
+      @response = 'Not Available!'
+    end
+  end
 
 end
--- spec/helpers/tools_helper_spec.rb
+++ spec/helpers/tools_helper_spec.rb
@@ -11,5 +11,35 @@ require 'spec_helper'
 #   end
 # end
 describe ToolsHelper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'availability' do
+    let(:user) { create(:user) }
+    let(:tool) { create(:tool) }
+
+    it 'should return "available" if the tool status == available' do
+      expect(helper.availability(tool.id, user.id)).to eq 'Available!'
+    end
+
+    it 'should return "That tool is here for you!" when the tool is on hold and has the user.id' do
+      tool.update(status: 'on hold')
+      tool.update(user_id: user.id)
+
+      expect(helper.availability(tool.id, user.id)).to eq 'That tool is here for you!'
+    end
+  end
+
+  
 end
+
+
+
+
+ # tool = Tool.find(tool_id)
+ #    if tool.status == 'available'
+ #      @response = 'Available!'
+ #    elsif tool.status == 'on hold' && user_id == tool.user_id
+ #      @reponse = 'That tool is here for you!'
+ #    elsif user_id == tool.user_id
+ #      @response  = "You've GOT this tool!"
+ #    else
+ #      @response = 'Not Available!'
+ #    end
