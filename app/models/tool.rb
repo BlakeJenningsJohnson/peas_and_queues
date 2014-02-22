84133584dc4904bd2959ab4b6c5a664e116da19f
Davida Marion <davidamarion@gmail.com>
2014-02-22 11:53:24 -0800

    toolz

--- app/models/tool.rb
+++ app/models/tool.rb
@@ -7,13 +7,14 @@ class Tool < ActiveRecord::Base
     if self.waitlists.count != 0
       Waitlist.remove_user_from_waitlist(id)
     end
+    self.update(status: 'checked out', user_id: user_id)
   end
 
   def return
-    if self.waitlists.count != 0
-      Waitlist.update_waitlist(self.id)
-    else
+    if self.waitlists.count == 0
       self.update(status: 'available', user_id: nil)
+    else
+      Waitlist.update_waitlist(self.id)
     end
   end
 
@@ -21,7 +22,6 @@ class Tool < ActiveRecord::Base
     tool = Tool.find(tool_id)
     if action == 'rent'
       tool.rent
-      tool.update(status: 'checked out', user_id: user_id)
     elsif action == 'return'
       tool.return
     elsif action == 'waitlist'
@@ -30,22 +30,3 @@ class Tool < ActiveRecord::Base
   end
 
 end
-
-
- # def availability(tool_id, user_id=nil)
- #    tool = Tool.find(tool_id)
- #    list = Waitlist.array_of_user_ids(tool_id)
-
- #    elsif user_id == tool.user_id
- #      @availability  = "You've GOT this tool!"
- #      @status = 'Return it!'
- #      @action = 'return'
- #    elsif list.include? user_id
- #      @availability = 'queue'
- #      @status = "You're on the waitlist! We'll let you know when you're number on the on the queue."
- #    else
- #      @availability = 'Not Available!'
- #      @status = 'Get on the waitlist!'
- #      @action = 'waitlist'
- #    end
- #  end
